72d6faab35c3b0238f11667e14cfc8482f419a0f
Peerapat Junkhaow <peerapat40595@gmail.com>
2020-04-16 08:51:53 +0700

    Updates

---
+++ precache-manifest.8b3d1e28539f69d21baa327df08e1aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0bfc8344f943088924c72d551b3824",
+    "url": "/AmathRandomRackGame/index.html"
+  },
+  {
+    "revision": "15999933eaab3f9e5d4d",
+    "url": "/AmathRandomRackGame/static/css/main.7b1466b3.chunk.css"
+  },
+  {
+    "revision": "b87bdb5e81e1b7819834",
+    "url": "/AmathRandomRackGame/static/js/2.02718d23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AmathRandomRackGame/static/js/2.02718d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15999933eaab3f9e5d4d",
+    "url": "/AmathRandomRackGame/static/js/main.a9492199.chunk.js"
+  },
+  {
+    "revision": "bfe43733d765672f52b6",
+    "url": "/AmathRandomRackGame/static/js/runtime-main.d4ae8806.js"
+  }
+]);
